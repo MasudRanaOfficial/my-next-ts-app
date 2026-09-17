5a7a2124d5dc59e62c8fbf385695b4ae343ec6aa
Masud Rana <masudrana.arh@gmail.com>
2026-09-17 09:04:03 +0600

    add: update AboutPage with additional images and configure remote image patterns

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,6 +5,14 @@ const AboutPage = () => {
   return (
     <div>
       <h2>About Us</h2>
+
+      <Image
+        src="https://images.unsplash.com/photo-1789276392890-1bc1d0f04374"
+        alt="image"
+        width={300}
+        height={300}
+      ></Image>
+      <Image src="https://i.ibb.co.com/JjGpPttd/masud-github-readme-banner.png" alt="cover" width={300} height={300}></Image>
       <Image
         src="/images/arman.jpeg"
         alt="Arman Hossain"
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      // https://i.ibb.co.com/JjGpPttd/masud-github-readme-banner.png
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "**",
+        search: "",
+      },
+      {
+        protocol: "https",
+        hostname: "i.ibb.co.com",
+        port: "",
+        pathname: "/JjGpPttd/**",
+        search: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
